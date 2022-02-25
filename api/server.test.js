eb9dfc4ec4de942f1636e6a7373194b2f8232cae
Christwide Oscar <triple101.games@gmail.com>
2022-02-25 17:46:22 -0600

    Updated Tests and restructured

--- api/server.test.js
+++ api/server.test.js
@@ -2,6 +2,7 @@ const db = require('../data/dbConfig');
 const server = require("./server");
 const request = require("supertest");
 const Jokes = require("./jokes/jokes-data"); 
+const Users = require('./users/users-model');
 
 test('sanity', () => {
   expect(true).toBe(true)
@@ -17,7 +18,25 @@ afterAll(async () => {
   await db.destroy();
 });
 
+describe('Testing Users Model', () => {
+  it('can find a user by id', async() => {
+    await db('users').insert({username: 'Warewolf', password: '1234'});
+    await db('users').insert({username: 'Spiderman ', password: '12345'});
+    await db('users').insert({username: 'Energizer', password: '123456'});
+    let result = await Users.findById(1);
+    expect(result.username).toBe('Warewolf');
+  });
+  it('can find a user by username', async() => {
+    await db('users').insert({username: 'Warewolf', password: '1234'});
+    await db('users').insert({username: 'Spiderman', password: '12345'});
+    await db('users').insert({username: 'Energizer', password: '123456'});
+    let [result] = await Users.findBy({username: 'Energizer'});
+    expect(result.username).toBe('Energizer');
+  });
+});
+
 const papa = {username: 'Daredevil', password: '1234'}
+
 describe("Users Endpoints", () => {
   beforeEach(async () => {
     await request(server).post("/api/auth/register").send(papa);
@@ -36,14 +55,14 @@ describe("Users Endpoints", () => {
   });
 });
 
-describe("joke endpoint", () => {
-  test("jokes restricted before login", async () => {
+describe("Joke Endpoints", () => {
+  it("jokes restricted before login", async () => {
     const res = await request(server).get("/api/jokes");
     expect(res.status).toBe(401);
     expect(res.body.message).toMatch(/token required/i);
     
   });
-  test("jokes allowed after login", async () => {
+  it("jokes allowed after login", async () => {
     await request(server).post("/api/auth/register").send(papa);
     const res = await request(server)
       .post("/api/auth/login")
@@ -53,5 +72,45 @@ describe("joke endpoint", () => {
       .get("/api/jokes")
       .set({ Authorization: token });
     expect(jokes.body[0].joke).toEqual(Jokes[0].joke);
+  });
+});
+
+describe('[POST] /api/auth/login', () => {
+  test('responds with correct status and message on invalid credentials', async() => {
+    let result = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'Captain Marvel', password: 'foobar' });
+    expect(result.status).toBe(401);
+    expect(result.body.message).toMatch(/invalid credentials/i);
+  });
+
+  test('responds with correct status and message on valid credentials', async() => {
+    let result = await request(server)
+      .post('/api/auth/register')
+      .send(papa);
+    result = await request(server)
+      .post('/api/auth/login')
+      .send(papa);
+    expect(result.status).toBe(200);
+    expect(result.body.message).toMatch('Welcome back Daredevil');
+  });
+});
+
+describe('[POST] /api/auth/register', () => {
+  test('responds with the correct status and user is added to database', async() => {
+    let result = await request(server)
+      .post('/api/auth/register')
+      .send(papa);
+    expect(result.status).toBe(201);
+    result = await Users.findById(1);
+    expect(result.username).toBe('Daredevil');
+  });
+
+  test('responds with the correct status and message without username', async() => {
+    let result = await request(server)
+      .post('/api/auth/register')
+      .send({ password: 'foobar' });
+    expect(result.status).toBe(400);
+    expect(result.body.message).toMatch('username and password are required');
   });
 });

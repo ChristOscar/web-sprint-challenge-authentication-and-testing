d22e001efdcb84f80b647fd29cdfbebe1b9fcdd5
Christwide Oscar <triple101.games@gmail.com>
2022-02-25 14:02:37 -0600

    Readme questions /bcrypt and JWt / server.test WIP

--- README.md
+++ README.md
@@ -54,6 +54,17 @@ Your finished project must include all of the following requirements (further in
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics.
 
 1. Differences between using _sessions_ or _JSON Web Tokens_ for authentication.
+
+- Difference between using sessions and web tokens is that when using sessions, the users information that was created is stored on the server in a cookie. When using JSON Web Tokens that information is stored on the client side and sent in the header of each request.
+
 2. What does `bcryptjs` do to help us store passwords in a secure manner?
+
+- Make passwords more secure by hashing them upon creation. It repeats the process many times as decided on. This information is held in process.`process.env.BCRYPT_ROUNDS`
+
 3. How are unit tests different from integration and end-to-end testing?
+
+- Unit tests are different, they are designed to test each piece of the code in isolation. Whereas integration and end-to-end testing focuses on different parts of the code working together
+
 4. How does _Test Driven Development_ change the way we write applications and tests?
+
+- TDD changes the way we write our applications and tests because we begin by writing a test that will fail. Making it pass with the smallest amount of code we can until it is testing exactly what we want. Then we can refactor and retest to make sure it is all working.
--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,10 @@
 const db = require('../data/dbConfig');
+const server = require("./server");
+const request = require("supertest");
+const Jokes = require("./jokes/jokes-data"); 
 
 test('sanity', () => {
-  expect(false).toBe(false)
+  expect(true).toBe(true)
 })
 beforeAll(async () => {
   await db.migrate.rollback();
@@ -14,3 +17,39 @@ afterAll(async () => {
   await db.destroy();
 });
 
+const papa = {username: 'Daredevil', password: '1234'}
+describe("Users Endpoints", () => {
+  beforeEach(async () => {
+    await request(server).post("/api/auth/register").send(papa);
+  });
+  it("User is able to register", async () => {
+    let users;
+    users = await db("users");
+    expect(users).toHaveLength(1);
+  });
+
+  it("User is able to login", async () => {
+    const res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "Daredevil", password: "1234" });
+    expect(res.body.message).toMatch('Welcome back Daredevil');
+  });
+});
+
+describe("joke endpoint", () => {
+  test("jokes restricted before login", async () => {
+    const res = await request(server).get("/api/jokes");
+    expect(res.body.message).toMatch(/token required/i);
+  });
+  test("jokes allowed after login", async () => {
+    await request(server).post("/api/auth/register").send(papa);
+    const res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "Daredevil", password: "1234" });
+    const token = res.body.token;
+    const jokes = await request(server)
+      .get("/api/jokes")
+      .set({ Authorization: token });
+    expect(jokes.body[0].joke).toEqual(Jokes[0].joke);
+  });
+});

df2678a489fc38e13c6d2be4ddf1b7c1ba8ae7b2
Christwide Oscar <triple101.games@gmail.com>
2022-02-25 12:42:42 -0600

    Creating server test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,16 @@
-// Write your tests here
+const db = require('../data/dbConfig');
+
 test('sanity', () => {
   expect(false).toBe(false)
 })
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+beforeEach(async () => {
+  await db("users").truncate();
+});
+afterAll(async () => {
+  await db.destroy();
+});
+
